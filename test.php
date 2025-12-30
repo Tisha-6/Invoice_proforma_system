339b295093b1f4967bb96b537d79565c018283e5
Tisha-6 <tishapranami6@gmail.com>
2025-12-30 14:20:21 +0530

    update

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "invoice_system");
+if ($conn->connect_error) {
+    die("Fail: " . $conn->connect_error);
+}
+echo "Database Connected Successfully";
